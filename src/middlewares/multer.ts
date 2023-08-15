dcf16b32e758115b204f1ee959cd879d6ea7c2a9
Jasper2002 <earth1887@gmail.com>
2023-08-15 16:04:07 +0700

    Update multer.ts

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,14 +1,23 @@
 import multer, { diskStorage } from "multer";
 import { v4 as uuidv4 } from "uuid";
 import { Request } from "express";
+import fs from "fs";
+import path from "path";
 
 const storage = diskStorage({
   destination: function (req: Request, file: Express.Multer.File, cb) {
-    cb(null, "./src/public/images/");
+    const destinationPath = path.join(__dirname, "../public/images");
+
+    fs.mkdirSync(destinationPath, { recursive: true });
+
+    cb(null, destinationPath);
   },
   filename: function (req: Request, file: Express.Multer.File, cb) {
     const uniqueSuffix = uuidv4();
-    const sanitizedOriginalName = file.originalname.replace(/[^a-zA-Z0-9]/g, '_');
+    const sanitizedOriginalName = file.originalname.replace(
+      /[^a-zA-Z0-9]/g,
+      "_"
+    );
     const uniqueFilename = `${sanitizedOriginalName}-${uniqueSuffix}.jpg`;
     cb(null, uniqueFilename);
   },
